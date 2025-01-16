5a6dda565757f9b7e53a4ba9b02389a1aa076ee5
AsierMUI <asieravino@gmail.com>
2025-01-16 13:19:36 +0100

    movimiento

---
+++ Space2D/Assets/Scrips/MovimientoNave.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PruebaMovimiento : MonoBehaviour
+{
+
+
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
+    private Vector2 movement;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+
+    void Update()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    #region Input Methods
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    #endregion
+}
