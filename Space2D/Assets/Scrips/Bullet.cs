f4d3017ea959796b967691466f28765c92440699
AsierMUI <asieravino@gmail.com>
2025-01-28 11:44:13 +0100

    scripts

---
+++ Space2D/Assets/Scrips/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Bullet Configuration")]
+    [SerializeField] float speed = 10;
+    [SerializeField] float limitY;
+
+    void Update()
+    {
+        //Movimiento
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        //limitador de pantalla
+        if (transform.position.y >= limitY) gameObject.SetActive(false);
+    }
+}
+
