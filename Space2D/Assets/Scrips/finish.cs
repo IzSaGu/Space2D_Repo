f2c7f7719820caf77f6c31ae98986beb9aeb65de
IzSaGu <izansanchezguillem@gmail.com>
2025-01-31 16:49:39 +0100

    re

---
+++ Space2D/Assets/Scrips/finish.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class finish : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            collision.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+
+    }
+}
