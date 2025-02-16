f6d482ad8a5aab79810799db61ffe9f0288d70b9
AsierMUI <asieravino@gmail.com>
2025-02-16 17:52:16 +0100

    a

--- Space2D/Assets/Scrips/Player.cs
+++ Space2D/Assets/Scrips/Player.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
 
     [Header("Movement")]
-    [SerializeField] private float speed =4f;
+    [SerializeField] private float speed = 4f;
     private float direction;
-
 
     [Header("Jump")]
     [SerializeField] private float jumpForce = 4f;
@@ -21,19 +21,14 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         direction = Input.GetAxisRaw("Horizontal");
         Jump();
+        AnimateMovement();
     }
 
     private void FixedUpdate()
@@ -44,16 +39,37 @@ public class Player : MonoBehaviour
     private void Move()
     {
         rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+
+        if (direction < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (direction > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 
     private void Jump()
     {
         isGrounded = Physics2D.Raycast(checkGround.position, Vector2.down, raycastlength, groundLayer);
-        print(isGrounded); 
 
-        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            animator.SetBool("IsJumping", true);
             rb.velocity = Vector2.up * jumpForce;
+            isGrounded = false;
         }
+        else if (isGrounded)
+        {
+            animator.SetBool("IsJumping", false);
+        }
+    }
+
+    void AnimateMovement()
+    {
+        animator.SetBool("IsJumping", !isGrounded);
+
+        animator.SetBool("IsMoving", direction != 0);
     }
 }
